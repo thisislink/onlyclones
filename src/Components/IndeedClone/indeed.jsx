a69742c824783aeeb1c2e3a798dcab859fd17605
Daphne Link <daphnelink@gmail.com>
2021-02-24 22:29:16 -0800

    fixed broken background styling

--- src/Components/IndeedClone/indeed.jsx
+++ src/Components/IndeedClone/indeed.jsx
@@ -1,43 +1,47 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import indeedStyles from '../IndeedClone/indeed.module.css';
-import Navigation from '../Navigation/Navigation';
 
 const IndeedClone = () => {
     return (
-        <body>
-            <div>
-                <div className={indeedStyles["container"]}>
-                    <p className={indeedStyles["headerTitle"]}>Indeed</p>
-                    <nav>
-                        <Link to="#" className={indeedStyles["styleLinks"]}>Find Jobs</Link>
-                        <Link to="#" className={indeedStyles["styleLinks"]}>Company Reviews</Link>
-                        <Link to="#" className={indeedStyles["styleLinks"]}>Find Salaries</Link>
-                        <Link to="#" className={indeedStyles["styleLinks"]}>Upload your resume</Link>
-                        <Link to="#" className={indeedStyles["styleLinks"]}>Sign in</Link>
-                        <Link to="#" className={indeedStyles["styleLinks"]}>Employers / Post Job</Link>
+        <>
+            <div className={indeedStyles["indeed-clone"]}>
+                <div>
+                    <div className={indeedStyles["navContainer"]}>
+                        <p>Indeed</p>
+                        <nav className={indeedStyles["styleLinks"]}>
+                            <ul>
+                                <li><Link to="/">Find Jobs</Link></li>
+                                <li><Link to="/all-clones">Company Reviews</Link></li>
+                                <li><Link to="#">Find Salaries</Link></li>
+                            </ul>
+                            <ul className={indeedStyles["styleTopNavRight"]}>
+                                <li><Link to="#">Upload your resume</Link></li>
+                                <li><Link to="#" className={indeedStyles["styleSignin"]}>Sign in</Link></li>
+                                <li><Link to="#">Employers / Post Job</Link></li>
+                            </ul>
+                        </nav>
 
-                    </nav>
-                    <Navigation className={indeedStyles["styleLinks"]} />
+                    </div>
+
+                    <section>
+                        <form>
+                            <input type="text" autocomplete="off" placeholder="What" />
+                            <input type="text" autocomplete="off" placeholder="Where" />
+                            <input type="submit" value="Find Jobs" />
+                        </form>
+                    </section>
+
+                    <section>
+                        <p>Post your resume - It only takes a few seconds</p>
+                    </section>
+
+                    <section>
+                        <p>Employers: get started - post a job, search resumes, and more</p>
+                    </section>
                 </div>
-
-                <section>
-                    <form>
-                        <input type="text" autocomplete="off" placeholder="What" />
-                        <input type="text" autocomplete="off" placeholder="Where" />
-                        <input type="submit" value="Find Jobs" />
-                    </form>
-                </section>
-
-                <section>
-                    <p className={indeedStyles["post-resume-text"]}>Post your resume - It only takes a few seconds</p>
-                </section>
-
-                <section>
-                    <p className={indeedStyles["employers-text"]}>Employers: get started - post a job, search resumes, and more</p>
-                </section>
-            </div >
-        </body >
+            </div>
+        </>
     );
 }
 
--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import homeStyles from './home.module.css';
 import Navigation from './Components/Navigation/Navigation';
+import navStyles from './Components/Navigation/NavStyle.module.css';
 
 const Home = () => {
     useEffect(() => {
@@ -9,35 +10,37 @@ const Home = () => {
     })
 
     return (
-        <body>
-            <div className={homeStyles["style-background"]}>
-                <div className={homeStyles["container"]}>
-                    <p className={homeStyles["header-title"]}>Only Clones</p>
-                    <Navigation />
+        <>
+            <div className={homeStyles["homepage-netflix-clone"]}>
+                <div className={homeStyles["style-background"]}>
+                    <div className={homeStyles["container"]}>
+                        <p className={homeStyles["header-title"]}>Only Clones</p>
+                        <Navigation className={navStyles["style-links"]} />
 
-                    <button className={homeStyles["btn-signin"]}>Sign In</button>
+                        <button className={homeStyles["btn-signin"]}>Sign In</button>
 
-                </div>
-
-                <section className={homeStyles["hero-container"]}>
-                    <div>
-                        <h1 className={homeStyles["subtitle"]}>Improve Frontend Knowledge By Cloning Sites</h1>
-                        <h2 className={homeStyles["subtitle"]}>Code Available On <a href="https://github.com/thisislink/onlyclones" target="_blank">GitHub</a></h2>
                     </div>
 
-                    <h3 className={homeStyles["subtitle-email-text"]}>I recreate websites to improve my frontend skills. Enter your email and I'll send you a site clone idea every Tuesday.</h3>
-                    <form className="form-container">
-                        <div className="align-form">
-                            <input className={homeStyles["style-email-input"]} type="email" autocomplete="off" placeholder=" " />
-                            <button className={homeStyles["style-submit"]} type="submit">Get Started</button>
-                            <label className={homeStyles["placeholder-label"]}>Email address</label>
+                    <section className={homeStyles["hero-container"]}>
+                        <div>
+                            <h1 className={homeStyles["subtitle"]}>Improve Frontend Knowledge By Cloning Sites</h1>
+                            <h2 className={homeStyles["subtitle"]}>Code Available On <a href="https://github.com/thisislink/onlyclones" target="_blank">GitHub</a></h2>
                         </div>
 
-                    </form>
-                </section>
+                        <h3 className={homeStyles["subtitle-email-text"]}>I recreate websites to improve my frontend skills. Enter your email and I'll send you a site clone idea every Tuesday.</h3>
+                        <form className="form-container">
+                            <div className="align-form">
+                                <input className={homeStyles["style-email-input"]} type="email" autocomplete="off" placeholder=" " />
+                                <button className={homeStyles["style-submit"]} type="submit">Get Started</button>
+                                <label className={homeStyles["placeholder-label"]}>Email address</label>
+                            </div>
 
+                        </form>
+                    </section>
+
+                </div>
             </div>
-        </body >
+        </>
     );
 }
 
